59553ee20e927221cee832bc64641ef28cdb2868
kunal <kunal.py3@gmail.com>
2022-04-08 08:04:22 +0530

    Commit 2

--- app/src/main/java/com/kunal/iot2/MainActivity.java
+++ app/src/main/java/com/kunal/iot2/MainActivity.java
@@ -3,6 +3,14 @@ package com.kunal.iot2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference vehRef = database.getReference("vehicle");
+        Map<String, Integer> veh = new HashMap();
+
+        ToggleButton powerbtn = findViewById(R.id.togglePower);
+        ToggleButton headLightbtn = findViewById(R.id.toggleHeadlight);
+        ToggleButton hornbtn = findViewById(R.id.toggleHorn);
+        Button savebtn = findViewById(R.id.buttonSave);
+        Button findbtn = findViewById(R.id.buttonFind);
+
+        savebtn.setOnClickListener(view -> {
+            powerbtn.
+            veh.put("power", )
+        });
+
+        veh.put("horn",1);
+        veh.put("power",1);
+        veh.put("headlight",0);
+
+        vehRef.setValue(veh);
     }
 }
